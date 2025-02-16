94a934ac9b80fcb67b047dae2078aa4cc889031e
mayasharma604 <143367716+mayasharma604@users.noreply.github.com>
2025-02-16 16:16:08 -0500

    combined in one function

--- minimap/coordinates.py
+++ minimap/coordinates.py
@@ -1,11 +1,12 @@
 import re
 import numpy as np
 
-def extract_sensor_data(filename):
-    """Extracts acceleration (Ax, Ay, Az) from sensor file."""
+def compute_positions_from_file(filename, dt=0.01):
+    """Reads acceleration data from file, integrates it to estimate position."""
     accel_x, accel_y, accel_z = [], [], []
     pattern = re.compile(r"Ax=(-?\d+\.\d+).+?Ay=(-?\d+\.\d+).+?Az=(-?\d+\.\d+)")
 
+    # Read and extract acceleration data
     with open(filename, 'r') as file:
         for line in file:
             match = pattern.search(line)
@@ -15,33 +16,25 @@ def extract_sensor_data(filename):
                 accel_y.append(ay)
                 accel_z.append(az)
 
-    return accel_x, accel_y, accel_z
-
-def integrate_acceleration(accel_x, accel_y, accel_z, dt=0.01):
-    """Integrates acceleration to estimate velocity and position."""
+    # Integrate acceleration to estimate velocity and position
     n = len(accel_x)
     velocity_x, velocity_y, velocity_z = np.zeros(n), np.zeros(n), np.zeros(n)
     position_x, position_y, position_z = np.zeros(n), np.zeros(n), np.zeros(n)
 
     for i in range(1, n):
-        # Velocity = Previous velocity + (acceleration * time step)
         velocity_x[i] = velocity_x[i-1] + accel_x[i] * dt
         velocity_y[i] = velocity_y[i-1] + accel_y[i] * dt
         velocity_z[i] = velocity_z[i-1] + accel_z[i] * dt
 
-        # Position = Previous position + (velocity * time step)
         position_x[i] = position_x[i-1] + velocity_x[i] * dt
         position_y[i] = position_y[i-1] + velocity_y[i] * dt
         position_z[i] = position_z[i-1] + velocity_z[i] * dt
 
     return position_x, position_y, position_z
 
-# Read acceleration from file
+# Call the function
 filename = "accelgyro.txt"
-accel_x, accel_y, accel_z = extract_sensor_data(filename)
-
-# Integrate acceleration to get position
-positions_x, positions_y, positions_z = integrate_acceleration(accel_x, accel_y, accel_z)
+positions_x, positions_y, positions_z = compute_positions_from_file(filename)
 
 print("Estimated Positions (X, Y, Z):")
 print("X:", positions_x)
