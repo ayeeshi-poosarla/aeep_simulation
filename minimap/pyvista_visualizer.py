04610490fa035bff29dc6c581e01cd76833820ce
Bennett Ye <bennettye@gmail.com>
2025-02-23 15:45:22 -0500

    moving coordinates

--- minimap/pyvista_visualizer.py
+++ minimap/pyvista_visualizer.py
@@ -1,5 +1,6 @@
 import pyvista as pv
 import numpy as np
+import time
 
 def main():
     # Replace 'my_model.stl' with the path to your STL file
@@ -14,28 +15,48 @@ def main():
     translation_vector = new_origin - current_center
     mesh.translate(translation_vector)
 
-
+    coordinate_file = "minimap/coordinates.txt"
+    f = open(coordinate_file)
+    plotter = pv.Plotter()
+    plotter.show_axes()
+    plotter.add_mesh(mesh, color="white", show_edges=True, opacity=0.5)
+    marker_position = mesh.center
+    marker = pv.PolyData(np.array([marker_position]))
+    plotter.add_mesh(marker, color="red", render_points_as_spheres=True, point_size=25)
+    for line in f:
+        plotter.show(auto_close=False)
+        print("in loop")
+        line = f.readline()
+        coords = line.split(' ')
+        x = float(coords[0])
+        y = float(coords[1])
+        z = float(coords[2])
+        desired_marker_position = [x, y, z]
+        marker.points = np.array([desired_marker_position])
+        plotter.render()
+        time.sleep(0.5)
+        print("out loop")
 
 
     #set marker to specified location
-    desired_marker_position = [0.08, -0.12, -0.08]
-    marker = pv.PolyData(np.array([desired_marker_position]))
+    #desired_marker_position = [0.08, -0.12, -0.08]
+    #marker = pv.PolyData(np.array([desired_marker_position]))
     #the block below moves the marker position to the center 
     #marker_position = mesh.center
     #marker = pv.PolyData(np.array([marker_position]))
 
     # Create a PyVista plotter
-    plotter = pv.Plotter()
+    #plotter = pv.Plotter()
 
     # Add the mesh to the plotter
-    plotter.add_mesh(mesh, color="white", show_edges=True, opacity=0.5)
-    plotter.add_mesh(marker, color="red", render_points_as_spheres=True, point_size=25)
+    #plotter.add_mesh(mesh, color="white", show_edges=True, opacity=0.5)
+    #plotter.add_mesh(marker, color="red", render_points_as_spheres=True, point_size=25)
 
     # Optional: show axes indicator in the 3D window
-    plotter.show_axes()
+    #plotter.show_axes()
 
     # Display the interactive window
-    plotter.show()
+    #plotter.show()
 
 if __name__ == "__main__":
     main()
