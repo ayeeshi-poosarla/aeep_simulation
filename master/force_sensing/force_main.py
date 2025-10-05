13c5d21357da030559b6f1f7a1092433c9595614
Bennett Ye <bennettye@gmail.com>
2025-10-05 17:49:02 -0400

    Added data storage

--- master/force_sensing/force_main.py
+++ master/force_sensing/force_main.py
@@ -1,15 +1,28 @@
 from force_reader_threading import get_latest_angles, start_serial_thread, stop_serial_thread
 from quadrant_detection import determine_quadrant
 import time
+import os
+import shutil
 
 start_serial_thread()
+
+def store_data(file_path, name):
+    directory_path = "bootcamp_data/" + name
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    
+    shutil.move(file_path, os.path.join(directory_path, os.path.basename(file_path)))
+    
 
 def scan_angles():
     while True:
         n,s,e,w, = get_latest_angles()
+        with open("output.txt", "w") as f:
+            f.write()
         print(determine_quadrant(n,s,e,w))
         time.sleep(0.2)
         
 
 if __name__ == "__main__":
-    scan_angles()
+    store_data("test2.txt", "joe")
+    #scan_angles()
