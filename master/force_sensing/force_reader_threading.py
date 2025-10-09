bbac4c3e7ab6cfa8cceaee72de4eea876312673d
kmhui28 <khui6@jh.edu>
2025-10-08 20:22:48 -0400

    Update force_reader_threading.py

--- master/force_sensing/force_reader_threading.py
+++ master/force_sensing/force_reader_threading.py
@@ -2,9 +2,9 @@ import serial
 import time
 import re
 from quadrant_detection import determine_quadrant
-from threading import Thread
+from threading import Thread, Lock
 
-pattern = re.compile(r"(\w+):\s*(-?\d+(?:\.\d+)?)")
+pattern = re.compile(r"(\w+):(-?\d+(?:\.\d+)?)")
 
 latest_angles = (0.0, 0.0, 0.0, 0.0)
 
@@ -12,13 +12,15 @@ def parse(data: str):
     matches = pattern.findall(data)
     return {d: float(v) for d, v in matches}
 
-def serial_loop(port='/dev/ttyACM0', baud_rate=115200):
+def serial_loop(port='/dev/arduino_flex', baud_rate=9600):
     """ Continuously read serial and update latest_angles """
     global latest_angles, stop_flag
     try:
-        with serial.Serial(port, baud_rate, timeout=0.01) as arduino:
+        with serial.Serial(port, baud_rate, timeout=0.5) as arduino:
             while True:
                 inline = arduino.readline().decode('utf-8', errors="ignore").strip()
+                #print(inline)
+                #time.sleep(0.2)
                 if not inline:
                     continue
                 dire = parse(inline)
@@ -26,12 +28,13 @@ def serial_loop(port='/dev/ttyACM0', baud_rate=115200):
                     north = dire.get("North", 0.0)
                     south = dire.get("South", 0.0)
                     east = dire.get("East", 0.0)
-                    west = dire.get("West", 0.0)
+                    west = dire.get("West",0.0)
+            
                     latest_angles = (north, south, east, west)
     except Exception as e:
         print(f"[Serial error] {e}")
 
-def start_serial_thread(port='/dev/ttyACM0', baud_rate=115200):
+def start_serial_thread(port='/dev/arduino_flex', baud_rate=9600):
     thread = Thread(target=serial_loop, args=(port, baud_rate), daemon=True)
     thread.start()
     return thread
@@ -43,3 +46,13 @@ def stop_serial_thread():
     """ Signal the serial loop to stop """
     global stop_flag
     stop_flag = True
+def scan_angles():
+    while True:
+        n,s,e,w = get_latest_angles()
+        quadrant = determine_quadrant(n,s,e,w)
+        print(n,s,e,w, quadrant)
+        time.sleep(0.2)
+
+if __name__ == "__main__":
+    start_serial_thread()
+    scan_angles()
