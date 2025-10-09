e69759179b0afa19ab6e2ef7fbb7d19a62c5e7c2
kmhui28 <khui6@jh.edu>
2025-10-08 20:23:34 -0400

    Update force_main.py

--- master/force_sensing/force_main.py
+++ master/force_sensing/force_main.py
@@ -1,12 +1,13 @@
-from force_reader_threading import get_latest_angles, start_serial_thread
+from force_reader_threading import get_latest_angles, start_serial_thread as start_force_thread, stop_serial_thread as stop_force_thread
 from quadrant_detection import determine_quadrant
-from conductive_reader_threading import *
+from conductive_reader_threading import get_latest_sheet, start_serial_thread as start_conductive_thread, stop_serial_thread as stop_conductive_thread
 import time
 import os
 import shutil
 import csv
 
-start_serial_thread()
+start_force_thread()
+start_conductive_thread()
 
 def store_data(file_path, name):
     directory_path = "bootcamp_data/" + name
@@ -25,9 +26,9 @@ def scan_angles():
         force_writer = csv.writer(f2)
 
         if new_file_1:
-            quadrant_writer.writerow(["timestamp", "quadrant"])
+            quadrant_writer.writerow(["timestamp", "quadrant", "bend_angle", "N", "S", "E", "W"])
         if new_file_2:
-            force_writer.writerow(["timestamp", "N", "S", "E", "W"])
+            force_writer.writerow(["timestamp", "force_Array"])
 
         while True:
             n, s, e, w = get_latest_angles()
@@ -36,14 +37,19 @@ def scan_angles():
             timestamp = time.time()
 
             print(quadrant)
-            quadrant_writer.writerow([timestamp, quadrant])
+            print(latest_sheet)
+            quadrant_writer.writerow([timestamp, quadrant, n, s, e, w])
             force_writer.writerow([timestamp, *latest_sheet])
             f1.flush()
             f2.flush()
 
-            time.sleep(0.2)
+            time.sleep(0.5)
         
 
 if __name__ == "__main__":
-    store_data("test2.txt", "joe")
-    #scan_angles()
+    ID = "f1"
+    try:
+        scan_angles()
+    except KeyboardInterrupt:
+        store_data("quadrant_log.csv", ID)
+        store_data("force_log.csv", ID)
